6143f7ebfa023023d6572f96248bd21b64e82554
jackwang <wangxichao001@gmail.com>
2017-09-04 16:32:13 +0800

    first add

---
+++ src/pages/home/components/test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ d }}
+    </div>
+</template>
+
+<script>
+    const test = {
+        name: 'test',
+        data: function () {
+            return {
+                d: 'you data from test.vue file'
+            }
+        }
+    }
+
+    export default test
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ test/unit/specs/Hello.spec.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue'
+// import Hello from '@/components/Hello'
+//
+// describe('Hello.vue', () => {
+//   it('should render correct contents', () => {
+//
+//   })
+// })
